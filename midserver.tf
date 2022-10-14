b153286b49e6733ba18aea0b50f019c343e43b25
Anthony Angelo <anthonyangelo@yahoo.com>
2022-10-14 00:33:22 -0400

    Update

--- midserver.tf
+++ midserver.tf
@@ -95,7 +95,7 @@ resource "azurerm_linux_virtual_machine" "midserver-vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.midserver-nic.id]
-  size                  = "Standard_D2_v3"
+  size                  = "Standard_DS2_v2"
   os_disk {
     name                 = "midserver-os"
     caching              = "ReadWrite"
